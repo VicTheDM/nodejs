48b043b83d240157d973391c76131ee8e3cddd59
Victor <victorsandovalsoftwaredev@gmail.com>
2023-10-07 10:09:25 -0600

    2

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
-const http = require('http');
-const PORT = 3000;
+const express = require('express');
+const cors = require('cors');
+const { PORT } = require('./src/config/properties');
+const connectLocalDB = require('./src/config/mongoDBAtlas');
+const app = express();
+connectLocalDB(); // Conexion con la base de datos
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
+app.use(cors());
+
+app.use(express.json()); // parse application/json
+
+app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
+
+// Rutas
+app.use('/api', require('./src/routes/login.routes'));
+app.use('/clientes', require('./src/controllers/clientes'));
+app.use('/inventario', require('./src/controllers/inventario'));
+app.use('/recetas', require('./src/controllers/recetas'));
+app.use('/bebidas', require('./src/controllers/bebidas'));
+
+// Servidor
+app.listen(PORT, () => console.log(`Escuchando por el puerto ${PORT}`) );
+
